36d0a119765cbbe5c4c0d0225a080567052e9749
Isaac Wu <isaacwu2000@gmail.com>
2025-10-02 11:51:54 -0400

    installed firebase

--- CAC2526/src/main.jsx
+++ CAC2526/src/main.jsx
@@ -3,6 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAV7FgcAZlVueothF6ij4uqppxl7eKX2aE",
+  authDomain: "app-challenge-2526-dev.firebaseapp.com",
+  projectId: "app-challenge-2526-dev",
+  storageBucket: "app-challenge-2526-dev.firebasestorage.app",
+  messagingSenderId: "363511042243",
+  appId: "1:363511042243:web:e311e2fd92cfac22751276"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
